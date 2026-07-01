06d1353d96aaa9e8b50df281b314b24e60edec9d
Gabriel Alvarenga Bastos <alvarengabriel04@gmail.com>
2026-07-01 08:31:43 -0300

    Deletando arquivos desnecessários
    Os arquivos que pegamos como modelo do professor serão melhor aproveitados posteriormente... Não há necessidade de manter isso upado no GitHub

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    if (!localStorage.clienteAutenticado) {
-        window.location.href = "login.html";
-    }else{
-        window.location.href = "menu.html";
-    }
-    
-})
-
