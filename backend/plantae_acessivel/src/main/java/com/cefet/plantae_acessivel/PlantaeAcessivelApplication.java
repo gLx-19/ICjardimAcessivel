b6a4c2bc126efc34ac3ddd716d517e67f8346650
Gabriel Alvarenga Bastos <alvarengabriel04@gmail.com>
2026-07-01 14:28:05 -0300

    Estrutura inicial do backend com Spring Boot
    Adição do projeto do Spring Initializr e configuração do application.properties para conexão com o banco de dados MySQL.

---
+++ backend/plantae_acessivel/src/main/java/com/cefet/plantae_acessivel/PlantaeAcessivelApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.plantae_acessivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantaeAcessivelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantaeAcessivelApplication.class, args);
+	}
+
+}
---
+++ backend/plantae_acessivel/src/test/java/com/cefet/plantae_acessivel/PlantaeAcessivelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.plantae_acessivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantaeAcessivelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
